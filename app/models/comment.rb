0e8c5c523b1d08bad40dcaf8d154bd89a544b4df
Diego Camacho <hackvan@gmail.com>
2017-09-25 12:55:24 -0500

    Implement a barnav for application layout
    	- Set a length maximum size to Comment's body.
    	- Make a partial for the navbar and use it on application layout.
    	- Make a partial for custom alerts on the site.
    	- Add some visual elements from Stackoverflow.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 255 }
 
   scope :order_by_oldest, -> { order("id asc") }
 
