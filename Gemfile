be64485ecfd9358c210f1be24497889c67ff4d6a
Diego Camacho <hackvan@gmail.com>
2017-09-25 13:32:02 -0500

    Implement the functionality of markdown on Questions and Answers
      - Add Redcarpet gem for markdown.
      - Add a markdown function helper.
      - Implement markdown to show Questions and Answers body.

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails', '~> 4.1', '>= 4.1.1'
 gem 'xmlrpc', '~> 0.3.0'
 gem 'gravatar-ultimate', '~> 2.0'
+gem 'redcarpet', '~> 3.4'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def markdown(text)
+    options = { hard_wrap: true,
+                filter_html: false,
+                autolink: true,
+                no_intra_emphasis: true,
+                fenced_code_blocks: true,
+                lax_html_blocks: true,
+                strikethrough: true,
+                superscript: true
+              }
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+    return markdown.render(text).html_safe
+  end
 end
